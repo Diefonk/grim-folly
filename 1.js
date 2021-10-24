bf7cfe7d5a62e15795f150163d4b240dfc72c024
Diefonk <thediefonk@gmail.com>
2021-10-24 21:26:38 +0200

    Wrote some of the first story

--- 1.js
+++ 1.js
@@ -1,5 +1,59 @@
 var one = {};
 
 one.start = {
-	text: "Start of 1"
-}
+	text: "You are sitting in your living room reading a book when the PHONE starts ringing. You consider just going out your front door, which is to the WEST.",
+	phone: {
+		text: "It is one of those newfangled phones you can talk into.",
+		talk: "\"Hello?\"\n\"Hi, I'm calling from the power company. Is now a good time to talk?\"\n\"Yes.\"\n\"Okay, I'll call back later then.\""
+	},
+	west: "garden"
+};
+
+one.garden = {
+	text: ""
+};
+
+one.bus1 = {
+	text: "You come to a bus stop where JOHN is waiting for the bus.",
+	john: {
+		text: "He is wearing his usual suit and has his usual suitcase on the ground next to him.",
+		talk: "\"There are two kinds of people in this world: those who believe there are two kinds of people, and those who believe that each individual is far too unique and complex for them to be divided into two groups.\""
+	}
+};
+
+one.bus2 = {
+	text: "You come to a bus stop where JOHN is waiting for the bus.",
+	john: {
+		text: "He is wearing his usual suit and has his usual suitcase on the ground next to him.",
+		talk: "\"The colours of this world are all just different shades of one colour in another world.\""
+	}
+};
+
+one.bus3 = {
+	text: "You come to a bus stop where JOHN is waiting for the bus.",
+	john: {
+		text: "He is wearing his usual suit and has his usual suitcase on the ground next to him.",
+		talk: "\"Everything exists.\""
+	}
+};
+
+one.bus4 = {
+	text: "You come to a bus stop where JOHN is waiting for the bus.",
+	john: {
+		text: "He is wearing his usual suit and has his usual suitcase on the ground next to him.",
+		talk: "\"The thought that our entire universe is just a computer simulation is pretty common, but what if it is far less advanced than that? What if our entire universe is just a simple math example drawn on some multidimensional blackboard by some multidimensional math teacher, and me saying this is just a math joke?\""
+	}
+};
+
+one.bus5 = {
+	text: "You come to a bus stop where JOHN is waiting for the bus.",
+	john: {
+		text: "He is wearing his usual suit and has his usual suitcase on the ground next to him.",
+		talk: "\"Hi.\""
+	}
+};
+
+one.end = {
+	text: "Despite the futility of it all, your chase continues. Will it ever end? Will any of our chases ever end? Are we all doomed to live our foolish lives until we die our foolish deaths? Does anyone have the answer to these questions? Whoever might explain this grim folly?",
+	end: true
+};
